ed2c9272e7bdbda49981d30b1fe0f92ef21ba151
rodrigore <rodrigopcg@gmail.com>
2016-01-05 17:09:36 -0300

    Bootstrap y Jquery en gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,11 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
   mix.sass('app.scss');
+
+  mix.scripts([
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+  ]);
+
+  mix.version(['css/app.css', 'js/all.js']);
 });

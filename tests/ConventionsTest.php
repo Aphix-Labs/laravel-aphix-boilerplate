ae389c8f9d698b8eeea84e5473decc16a9d846f5
rodrigore <rodrigopcg@gmail.com>
2016-01-08 14:38:10 -0300

    Regla de convencion para trait

--- tests/ConventionsTest.php
+++ tests/ConventionsTest.php
@@ -48,16 +48,16 @@ class ConventionsTest extends PHPUnit_Framework_TestCase
 
     private function file_get_php_classes($filepath) {
         $php_code = file_get_contents($filepath);
-        $classes = $this->get_php_classes($php_code);
-        return $classes;
+        return $this->get_php_classes($php_code);
     }
 
     private function get_php_classes($php_code) {
-        $classes = array();
+        $classes = [];
         $tokens = token_get_all($php_code);
         $count = count($tokens);
         for ($i = 2; $i < $count; $i++) {
-            if (   $tokens[$i - 2][0] == T_CLASS
+            if (   ($tokens[$i - 2][0] == T_CLASS
+                || $tokens[$i - 2][0] == T_TRAIT)
                 && $tokens[$i - 1][0] == T_WHITESPACE
                 && $tokens[$i][0] == T_STRING) {
 

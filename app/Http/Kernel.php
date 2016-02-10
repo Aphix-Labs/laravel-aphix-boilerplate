0cf7b8a88a263ac4b8e4453d9e96747c37e1c203
rodrigore <rodrigopcg@gmail.com>
2016-02-10 15:02:02 -0300

    Habilita extension clockwork

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,6 +29,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+            \Clockwork\Support\Laravel\ClockworkMiddleware::class,
         ],
 
         'api' => [
